a66ba31cec2fcd1e43f1544ecb0798fe028bb9d2
Gagandeep Singh <sainigagan82@gmail.com>
2022-12-06 20:20:09 -0700

    Add files via upload

---
+++ js/getid.js
@@ -0,0 +1,12 @@
+var idvalue1;
+function getid(idvalue)
+{
+	//alert(idvalue);
+	var queryString = "?para1=" + idvalue;
+	window.location.href = "singlegift.html" + queryString;
+}
+function returnid()
+{
+	
+	//alert("value="+idvalue1);
+}
---
+++ js/productname.js
@@ -0,0 +1,219 @@
+let products = 
+[
+    {
+        'id': 31,
+        'name': 'Parrot Painting',
+        'description': '16x20 inches DIY 5D Square Diamond Painting Cross Stitch Kits Parrot Handmade Full Drill Painting with Diamonds Diamond Art for Adults Mosaic Embroidery Rhinestones Family Ornament',
+        'price': 59.99,
+        'pic': 'images/product/31.0.jpg',
+        'qty_in_stock':2
+    },
+    {
+        'id': 32,
+        'name': 'Buddha Painting',
+        'description': 'Healifty 5D Diamond Painting Buddha Cross Stitch Diamond Embroidery Painting Handmade Cross Stitch Printing Craftwork Party Wall Decoration for Home Living Room Birthday Gifts',
+        'price': 49.99,
+        'pic': 'images/product/32.0.jpg',
+        'qty_in_stock': 133
+    },
+    {
+        'id': 33,
+        'name': 'Horse Paiting',
+        'description': 'Manchester United Extra Jersey, black, sponsored by Chevrolet',
+        'price': 54.99,
+        'pic': 'images/product/33.0.jpg',
+        'qty_in_stock': 544
+    },
+    {
+        'id': 34,
+        'name': 'Asdam Art',
+        'description': 'Handmade item by Asdam Art  Size: 60*90cm(24*36inch)----- All images on Asdam Art store are real shot, to ensure that the paintings you get are same with the pictures they showed. Materials: acrylic, impasto, texture, metallic, palette knife, large canvas, Mixed Media Acrylic. Dominant colors:red,white,brown, beige, grey.',
+        'price': 129.99,
+        'pic': 'images/product/34.0.jpg',
+        'qty_in_stock': 14
+    },
+    {
+        'id': 35,
+        'name': 'Acrylic Painting',
+        'description': 'PERFECT OIL PAINTWORKS - Acrylic Pigment have a bright color, not easy to fade, can save oil paintworks for a long time, the paintbrush is made of high-elasticnylon, it help you paint more comfortably HIGHT QUALITY CANVAS - Canvas is a linen for a special use of painting by numbers, the canvas color is natural and not perishable LIFE PARTNER - Digital oil painting can help you relieve stress. It also can cultivate your interest and patience',
+        'price': 204.99,
+        'pic': 'images/product/35.0.jpg',
+        'qty_in_stock': 655
+    },
+    {
+        'id': 36,
+        'name': 'Flower Painting',
+        'description': '5D Diamond Painting DIY Handmade Blue Flower Painting by Numbers Diamond Art Full Drill Embroidery Canvas Rhinestone Cross Stitch Crystal Craft Wall Art Living Room Bedroom Home Decor 12X16 inches',
+        'price': 40.99,
+        'pic': 'images/product/36.0.jpg',
+        'qty_in_stock': 321
+    },
+	   {
+        'id': 37,
+        'name': 'Oil Painting',
+        'description': 'Hand Painted Modern Horse Oil Painting on Canvas Handmade Abstract Animal Wall Art for Decor (Horse, 24x36 inch) Cross Stitch Crystal Craft Wall Art Living Room Bedroom Home Decor 12X16 inches',
+        'price': 49.99,
+        'pic': 'images/product/6.0.jpg',
+        'qty_in_stock': 321
+    },
+	{
+        'id': 11,
+        'name': 'Girl Statue',
+        'description': 'Disney Showcase 4045772 DSSHO Anna Figurine  Brand	Disney Showcase Material	Resin Product Dimensions	13.21 x 10.01 x 20.32 cm; 340 Grams',
+        'price': 49.99,
+        'pic': 'images/product/11.0.jpg',
+        'qty_in_stock': 321
+    },
+	{
+        'id': 12,
+        'name': 'Eagle Statue',
+        'description': 'NONEMEY Mirrored Clear Acrylic Display Case Assemble Countertop Box Stand Organizer Dustproof Protection Showcase Acrylic Display Stands for Toys Collectibles (8×8×12inch；20×20×30cm)',
+        'price': 59.99,
+        'pic': 'images/product/12.0.jpg',
+        'qty_in_stock': 321
+    },
+	{
+        'id': 13,
+        'name': 'Angle Statue',
+        'description': 'NATURSTON Handcrafted Handmade angel Statue Natural Labradorite Detailed Carving Labradorite Stone Figurine Healing Gift Dark Blue 2.5-2.9 Brand: NATURSTON',
+        'price': 89.99,
+        'pic': 'images/product/13.0.jpg',
+        'qty_in_stock': 321
+    },
+	{
+        'id': 14,
+        'name': 'Guardian Statue',
+        'description': 'PTC 6.25 Inch Resin Medieval Sitting Guardian Gargoyle with Wings Statue 6 1/4 in. Tall, 5 1/4 in. Wide, 5 1/4 in. Deep Made of Cold Cast Resin Great House-Warming Gift',
+        'price': 79.99,
+        'pic': 'images/product/14.0.jpg',
+        'qty_in_stock': 321
+    },
+	{
+        'id': 15,
+        'name': 'Holy Family Statue',
+        'description': 'Joseph Studio Renaissance Holy Family Water Font Catholic Wall Decoration 62885 Brand	Joseph Studio Material	Stone Resin Product Dimensions	10.16 x 6.35 x 20.32 cm; 226.8 Grams',
+        'price': 99.99,
+        'pic': 'images/product/15.0.jpg',
+        'qty_in_stock': 321
+    },
+	{
+        'id': 16,
+        'name': 'Chinese Gods Statue',
+        'description': ' FENGSHUI Handmade Ancient Chinese Gods Statue Decoration Gift Brass Finish Good Luck God Collectible Figurines (Yvdi) About this item Best distinctive gift, a great personal collection Golden color, made of copper, Length 6.7, width 5.9, height 11.4 The shape is very unique, Pure handmade, excellent workmanship Refresh the decor appeal of short spaces on tables and shelves Will look great on a shelf, table or mantel',
+        'price': 89.99,
+        'pic': 'images/product/16.0.jpg',
+        'qty_in_stock': 321
+    },
+	{
+        'id': 21,
+        'name': 'Steel Flower Stand',
+        'description': 'The flower frame is made of stainless steel raw materials, wear-resistant and corrosion-resistant, and the bright light process is used for a long time without discoloration. The open design of the flower stand makes it easy to reach the watering, which is conducive to plant growth and fully absorbs sunlight. The plant stand can be used as a multi-layer storage display stand for sundries, decorations, toys, shoes, books, plants. Plant racks can be placed in living rooms, bedrooms, family corners, gardens, balconies, yards, patios, decks, shops, trade fairs, etc.The plant stand is also the best holiday gift for mom, girlfriend, family birthday, wedding, and Christmas. Pet cats also like to jump on it to play!',
+        'price': 49.99,
+        'pic': 'images/product/21.0.jpg',
+        'qty_in_stock': 321
+    },
+	{
+        'id': 22,
+        'name': 'Flower Pot European American Hand-Painted',
+        'description': '- Indoor And Outdoor Flower Pots: Simple shape of exhibition space add size and visual diversity for outdoor space, and turn your beautiful garden and room into a natural plant paradise full of space crisscross and rich sense. - Process: high-scale cement steel plate pouring, anti-weathering sunscreen waterproofing process, the surface of the ceramic is hand-painted high temperature firing. - High-end ceramic mosaic Bold and thicker materials and more texture foot design make plant stand more stable. - Suitable for various scenes such, perfect for family green plant landscaping, office, home, shopping mart, hotel, bar, balcony garden, home gardening, coffee table display decorations. - ZHKY can provide quality warranty and amazon customer service, if there is any quality problem, please contact our seller service, absolute ZERO risk of this shopping.',
+        'price': 140.99,
+        'pic': 'images/product/22.0.jpg',
+        'qty_in_stock': 321
+    },
+	{
+        'id': 23,
+        'name': 'Flower Rack Plant Stand Multi Wood',
+        'description': 'Powerful bearings: This garden plant rack features strong bearing capacity. Stable design: The stand bending foot design increases the stability of the iron frame and provide more stable support. Convenient to drain with its tier hollowed design. Use of metal screw buckle solid connection, easy to install more stable and stable Carbonised anticorrosive pine, environmental protection without formaldehyde, smooth burr less, load capacity I wish you a happy shopping! welcome next time!Thank you for your trust and support! I wish you a happy life！',
+        'price': 89.99,
+        'pic': 'images/product/23.0.jpg',
+        'qty_in_stock': 321
+    },
+	{
+        'id': 24,
+        'name': '5 Tier Metal Plant Stand',
+        'description': 'This fascinating floral plant frame features a durable metal construction. Environmentally friendly and sturdy; each edge is carefully polished and has a smooth surface. No burrs, smooth touch will not hurt anyone Class 1 board has no odor, refuses formaldehyde, harms health, and is resistant to water and fire. The texture is clear and beautiful. The load is strong. The material will not cause any chemical pollution, nor will it cause harm to human health. Simply wipe with a cloth and do not use any detergent or soapy water. And the bracket is very durable and can be used for a long time. Simple structure, easy to install, easy and effortless, save time and effort',
+        'price': 99.99,
+        'pic': 'images/product/24.0.jpg',
+        'qty_in_stock': 321
+    },
+	{
+        'id': 25,
+        'name': 'All Solid Wood Flower Stand',
+        'description': 'Wood material: mahogany. Color classification: American cherry. The quality furniture from Europe contains freedom and unrestrainedness, dark and calm luxury, and gorgeous style reveals the noble and magnificent royal feeling. ★Decorative: a lovely way to display your flowers - colourful buds, green plants, herbs, vases, etc. ★Function: Home decoration, store items and save space. Suitable for multiple scenes, such as bedroom, kitchen, bathroom, living room, balcony, hallway, office, cafe.',
+        'price': 49.99,
+        'pic': 'images/product/25.0.jpg',
+        'qty_in_stock': 321
+    },
+	{
+        'id': 26,
+        'name': '4 Tier Plant Metal Stand Flower Pot',
+        'description': 'Creative design:Featuring with european style iron art design. Curved and smooth line make the the multi-layer metal planter shelf gorgeous, and also increases the stability of the iron frame Use in Multi-application: Widely used indoor and outdoor, garden, porch, balconies, hallways, kitchens, living room, bathrooms, flower shop, home, office to decorate dull spaces universal wheels：Plant display rack comes with 4 universal wheels, which is convenient to move around for decoration or cleaning. Move the position easily.Wheels with brakes allow it to be locked in place. These wheels can be detached with screwdriver, so you can choose to use this shelf with or without wheels. Reliable Construction： Made from stoving varnish painting metal frame, this budget -friendly piece construct this sturdy and scientific construction of curved, it can afford heavy items but occupy small areas Easy Assemble：Only need 2 steps to set it up. Compact design save place can greatly help you save space',
+        'price': 89.99,
+        'pic': 'images/product/26.0.jpg',
+        'qty_in_stock': 321
+    }
+ ]
+ const duplicateproduct=products;
+ //cart.addEventListener('click', cart1);
+ //reset.addEventListener('click', myfuction2);
+ function cart1()
+ {
+    alert("fuct1");
+ }
+ function myfuction2()
+ {
+    document.getElementById("cart2").innerHTML =0;
+    console.log(duplicateproduct)
+    console.log(products)
+    products=duplicateproduct;
+ }
+ function myfunction(idd)
+ {
+    let res = products.find((item) => {
+        return item.id == idd;
+    });
+    res.qty_in_stock=res.qty_in_stock-1;
+    console.log(res.qty_in_stock);
+    var number_of_product=document.getElementById("cart");
+    k=Number(number_of_product.querySelector("div span").innerHTML);
+    k=k+1;
+    document.getElementById("cart2").innerHTML=k;
+ }
+
+ function getname(idd)
+ { 
+    let res = products.find((item) => {
+        return item.id == idd;
+    });
+    return res.name;
+ }
+ function getimage(idd)
+ { 
+    let res = products.find((item) => {
+        return item.id == idd;
+    });
+    return res.pic;
+ }
+  function getdescribtion(idd)
+ { 
+    let res = products.find((item) => {
+        return item.id == idd;
+    });
+    return res.description;
+ } 
+ function getprice(idd)
+ { 
+    let res = products.find((item) => {
+        return item.id == idd;
+    });
+    return res.price;
+ }
+  function getqty(idd)
+ { 
+    let res = products.find((item) => {
+        return item.id == idd;
+    });
+    return res.qty_in_stock;
+ }
+
+ 
